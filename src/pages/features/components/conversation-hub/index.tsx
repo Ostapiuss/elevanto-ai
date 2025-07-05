0f9b40509f31f60138b1043e26bb001f6cb54957
ostap-okolovych <ostapokolovych@gmail.com>
2025-07-04 23:53:26 -0400

    temp deploy

--- src/pages/features/components/conversation-hub/index.tsx
+++ src/pages/features/components/conversation-hub/index.tsx
@@ -47,7 +47,7 @@ export default function ConversationHub() {
   const [value, setValue] = React.useState(0);
   const [selectedUser, setSelectedUser] = useState<ContactItem>(allUsers[0]);
 
-  const handleChange = (event: React.SyntheticEvent, newValue: number) => {
+  const handleChange = (_: React.SyntheticEvent, newValue: number) => {
     setValue(newValue);
   };
 
